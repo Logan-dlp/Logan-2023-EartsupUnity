0c686859f63d9d9edea770bfd6340e5fbde04255
Logan <pix.59700@gmail.com>
2023-11-30 16:52:56 +0100

    fire charge instance

--- Assets/_Core/InputSystem/ExoInputSystem/Scripts/PlayerBehaviour.cs
+++ Assets/_Core/InputSystem/ExoInputSystem/Scripts/PlayerBehaviour.cs
@@ -1,10 +1,14 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 [RequireComponent(typeof(InputEventButtonListener))]
 public class PlayerBehaviour : MonoBehaviour
 {
+    [SerializeField] private GameObject _fireCharge;
+    
     public void Shoot()
     {
-        Debug.Log("Shoot !");
+        Vector3 fireInstancePosition = transform.position + transform.up;
+        Instantiate(_fireCharge, fireInstancePosition, transform.rotation);
     }
 }
