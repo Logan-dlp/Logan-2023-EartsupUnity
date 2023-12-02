455ebe6a260db6875118662ee2cdb3fa58439777
Logan <pix.59700@gmail.com>
2023-12-02 19:07:30 +0100

    Destroy FireCharge instance

--- Assets/_Core/InputSystem/ExoInputSystem/Scripts/FireChargeMovement.cs
+++ Assets/_Core/InputSystem/ExoInputSystem/Scripts/FireChargeMovement.cs
@@ -6,21 +6,15 @@ using UnityEngine;
 public class FireChargeMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private float _destroyTime;
-
-    private void Start()
-    {
-        StartCoroutine(DestroyObjectInTime(_destroyTime));
-    }
+    [SerializeField] private float _timeToDestroy;
 
     private void Update()
     {
         transform.position += transform.up * Time.deltaTime * _speed;
     }
 
-    IEnumerator DestroyObjectInTime(float time)
+    private void OnBecameInvisible()
     {
-        yield return new WaitForSeconds(time);
         Destroy(gameObject);
     }
 }
