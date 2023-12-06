31778c468ccca567932312d47db35b7e729b3054
Logan <pix.59700@gmail.com>
2023-12-06 15:04:32 +0100

    Point Detection

---
+++ Assets/_Core/MathAppliquees/Scripts/PointDetection.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class PointDetection : MonoBehaviour
+{
+    [SerializeField] private Transform _pointTransform;
+    [SerializeField, Range(0, 10)] private float _radiusDetection;
+    
+    private void OnDrawGizmos()
+    {
+        Vector3 distance = _pointTransform.position - transform.position;
+        if (distance.sqrMagnitude < _radiusDetection * _radiusDetection)
+        {
+            Handles.color = Color.green;
+        }
+        else
+        {
+            Handles.color = Color.white;
+        }
+        
+        Handles.DrawWireDisc(transform.position, transform.forward, _radiusDetection);
+    }
+}
