5742e9d6b27ff9c3c58dfc896b41d1f663078397
Logan <pix.59700@gmail.com>
2023-12-14 15:09:54 +0100

    Add Button for scene index

---
+++ Assets/_Core/Editors/ExoEditor/Scripts/Editor/CustomEditorComponentEditor.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CustomEditor(typeof(CustomEditorComponent))]
+public class CustomEditorComponentEditor : Editor
+{
+    private SerializedProperty sampleTextProp;
+    private SerializedProperty sceneIndexProp;
+
+    private void OnEnable()
+    {
+        sampleTextProp = serializedObject.FindProperty("_sampleText");
+        sceneIndexProp = serializedObject.FindProperty("_sceneIndex");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        using (new EditorGUILayout.HorizontalScope())
+        {
+            if (GUILayout.Button("-1"))
+            {
+                sceneIndexProp.intValue--;
+                SceneManager.GetSceneByBuildIndex(sceneIndexProp.intValue);
+            }
+
+            if (GUILayout.Button("+1"))
+            {
+                sceneIndexProp.intValue++;
+                SceneManager.GetSceneByBuildIndex(sceneIndexProp.intValue);
+            }
+        }
+        EditorGUILayout.LabelField(sampleTextProp.stringValue);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
